b4177377d32b1c2d4323463c173ceb9fc30de1ca
karin224 <karin224@naver.com>
2018-02-27 23:03:04 +0900

    homework_0227

---
+++ hoabin/h180227_maxfunction.py
@@ -0,0 +1,19 @@
+def max (a, b):
+
+    if a > b :
+        print (a)
+
+    elif a < b :
+        print (b)
+
+    else :
+        print ("same")
+
+print ("max (1,1)")
+max (1,1)
+
+print ("max (2,7)")
+max (2,7)
+
+print ("max (8,4)")
+max (8,4)
