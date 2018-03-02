47f0c92517514d95008c22b80228c6ebd74ccbdc
paxpo <akdzktp1@naver.com>
2018-03-02 23:41:56 +0800

    Homework

---
+++ minseok/h180301_average.py
@@ -0,0 +1,21 @@
+i = 0
+k = 0
+while True:
+    k = k+1
+    if (k==1):
+        i = 101+i
+    elif (k==2):
+        i = 323+i
+    elif (k==3):
+        i = 121+i
+    elif (k==4):
+        i = 412+i
+    elif (k==5):
+        i = 236+i
+    elif (k==6):
+        i = 478+i
+    else:
+        i = 355+i
+        break
+
+print (i/k)
