f285bf6822100d9b370efb6c60d5069b5d03bf61
junholeephy <skyblue1293@naver.com>
2018-03-05 05:33:07 +0800

    soomin's homework

--- junho/h180304_list.py
+++ junho/h180304_list.py
@@ -10,7 +10,7 @@ def min_list(LIST):                          # 리스트를 input 했을때, 가
         temp = LIST[i]
         LIST[i] = Min
         LIST[index] = temp
-    print(LIST)
+    print("The ordered list would be :",LIST)
     return LIST
 
 
@@ -21,15 +21,21 @@ def five(LIST):
     for i in range(len(LIST)):
         if(LIST[i] % 5 ==0):
             List.append(LIST[i])
-    print(List)
+    print("No remaing if divided by 5 numbers would be :",List)
     return List
 
 
 def main():
     a = [125,421,327,380,728,645]
-    List = min_list(a)
-    List_five = five(List)
+    print("If the input list is : ",a)
+    List_a = min_list(a)
+    List_five_a = five(List_a)
+    print();
 
+    b = [100,200,302,123,312,454,235,234,245,632,673,100,564,200,453,345,743,342,765,753,234,123,76,12,543,673,76]
+    print("If the input list is : ",b);
+    List_b = min_list(b)
+    List_five_b = five(List_b)
 
 if __name__=="__main__":
     main()
